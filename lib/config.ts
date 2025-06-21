a05c1ed32ecf3729d5d2c5edddfc60fc550f1346
luanle09 <luan.le@karrostech.com>
2025-06-21 23:54:02 +0700

    m

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: 'LeanVita',
   domain: 'leanvita.online',
   description: 'Fitness & Fat Loss Tips Backed By Real Science.',
-  notionPageId: '217a2efa782d802a8ba3c51b473c4736',
+  notionPageId: '217a2efa782d80b1b45eeffdae753964',
   author: 'LeanVita Team',
   github: 'https://github.com/luanleb09/leanvita-fitness-blog'
 }
