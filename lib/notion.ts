09d21fe9a11bf4e355648f2cd866d90dc86127a9
luanle09 <luan.le@karrostech.com>
2025-06-22 00:24:31 +0700

    update notion

--- lib/notion.ts
+++ lib/notion.ts
@@ -2,6 +2,28 @@ import { Client } from '@notionhq/client'
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN })
 
+const databaseId = process.env.NOTION_DATABASE_ID || ''
+
+export async function getDatabase() {
+  const response = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+      property: 'Published',
+      checkbox: {
+        equals: true,
+      },
+    },
+    sorts: [
+      {
+        property: 'Date',
+        direction: 'descending',
+      },
+    ],
+  })
+
+  return response.results
+}
+
 export async function getBlogPosts(databaseId: string) {
   const response = await notion.databases.query({
     database_id: databaseId,
