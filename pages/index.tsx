57f37d250a88017b6011eb8d38e5069e57ac7fdb
luanle09 <luan.le@karrostech.com>
2025-06-23 18:26:34 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,6 @@ import Head from 'next/head';
 import { siteConfig } from '../lib/config';
 import { getBlogPosts } from '../lib/notion';
 
-export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getBlogPosts()
-  return { props: { posts }, revalidate: 60 }
-}
-
 
 type Post = {
   id: string;
